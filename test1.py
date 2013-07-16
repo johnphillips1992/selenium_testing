15da57efad49b27183ca700f8d252e24dc66f1f1
John Phillips <johnphillips@arch.tamu.edu>
2013-07-16 08:42:13 -0500

    Prints test time and number of errors at end of test.

--- test1.py
+++ test1.py
@@ -6,6 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 from selenium.webdriver.common.by import By
 import time
 
+start_millis = int(round(time.time() * 1000))
+
+errors = 0
+
 # Create a new instance of the Chrome driver
 chromedriver = "./chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
@@ -13,57 +17,27 @@ driver = webdriver.Chrome(chromedriver)
 driver.set_window_size(1300, 900)
 
 def test_site(site):
-    driver.set_window_size(1300, 900)
-
     driver.get(site)
     try:
         element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "footer")))
-        time.sleep(1)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(1)
-
-        driver.execute_script("window.scrollTo(0, 0);")
-        driver.set_window_size(800, 900)
-        time.sleep(1)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(1)
-
-        driver.execute_script("window.scrollTo(0, 0);")
-        driver.set_window_size(300, 900)
-        time.sleep(1)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(1)
         return True
-
     except:
         print site + " failed"
+        errors = errors + 1
         return False
 
-
 test_site("http://myaccount-dev.arch.tamu.edu")
-
 test_site("http://chc-dev.arch.tamu.edu")
-
 test_site("http://chsd-dev.arch.tamu.edu")
-
 test_site("http://chud-dev.arch.tamu.edu")
-
 test_site("http://colonias-dev.arch.tamu.edu")
-
 test_site("http://cosc-dev.arch.tamu.edu")
-
 test_site("http://creativity-dev.arch.tamu.edu")
-
 test_site("http://crs-dev.arch.tamu.edu")
-
 test_site("http://dept-dev.arch.tamu.edu")
-
 test_site("http://hrrc-dev.arch.tamu.edu")
-
 test_site("http://laup-dev.arch.tamu.edu")
-
 test_site("http://one-dev.arch.tamu.edu")
-
 test_site("http://payments-dev.arch.tamu.edu")
 
 driver.get("http://pypi-dev.arch.tamu.edu")
@@ -73,12 +47,15 @@ except:
     print "payments-dev.arch failed"
 
 test_site("http://symposium-dev.arch.tamu.edu")
-
 test_site("http://targetcities-dev.arch.tamu.edu")
-
 test_site("http://viz-dev.arch.tamu.edu")
-
 test_site("http://dev.arch.tamu.edu")
 
 driver.quit()
+
+end_millis = int(round(time.time() * 1000))
+total_millis = end_millis - start_millis
+print "Test time: " + str(float(total_millis)/1000) + " seconds"
+
+print str(errors) + " errors"
 
