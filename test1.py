18a00d6678c8cdbd556044b4e90c1c9566d2dcf1
John Phillips <johnphillips@arch.tamu.edu>
2013-07-16 09:41:57 -0500

    Added checks for header and site-top-menu classes. Removed error count print out.

--- test1.py
+++ test1.py
@@ -8,23 +8,25 @@ import time
 
 start_millis = int(round(time.time() * 1000))
 
-errors = 0
-
 # Create a new instance of the Chrome driver
 chromedriver = "./chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
-driver.set_window_size(1300, 900)
 
 def test_site(site):
     driver.get(site)
     try:
         WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "navbar")))
+        WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "header")))
+        WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "site-top-menu")))
+        WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "footer")))
         return True
     except:
         print site + " failed"
-        errors = errors + 1
         return False
 
 test_site("http://myaccount-dev.arch.tamu.edu")
@@ -41,7 +43,7 @@ test_site("http://laup-dev.arch.tamu.edu")
 test_site("http://one-dev.arch.tamu.edu")
 test_site("http://payments-dev.arch.tamu.edu")
 
-# pypi.arch does not have a footer tag
+# pypi.arch does not have a footer class
 # so we test for the table tag.
 driver.get("http://pypi-dev.arch.tamu.edu")
 try:
@@ -60,6 +62,4 @@ driver.quit()
 end_millis = int(round(time.time() * 1000))
 total_millis = end_millis - start_millis
 print "Test time: " + str(float(total_millis)/1000) + " seconds"
-
-print str(errors) + " errors"
 
