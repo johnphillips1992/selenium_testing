29a28895b43f8277214838c90daab01fb68bbbb1
John Phillips <johnphillips@arch.tamu.edu>
2013-07-17 08:39:44 -0500

    Removed unused import.

--- test1.py
+++ test1.py
@@ -1,6 +1,5 @@
 import os
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
