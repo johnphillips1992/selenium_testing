53d55d48a2e7f0ccca094e5bea5ba2fe3d1de8ed
John Phillips <johnphillips@arch.tamu.edu>
2013-07-26 14:25:11 -0500

    Changed to two tests, one for dev and one for production.

---
+++ test_production.py
@@ -0,0 +1,76 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import sys
+
+startMillis = int(round(time.time() * 1000))
+
+errors = 0
+
+# Create a new instance of the Chrome driver
+chromedriver = "./chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+
+def test_site(site):
+    global errors
+    driver.get(site)
+    try:
+        WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "navbar")))
+        WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "header")))
+        WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "site-top-menu")))
+        WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "footer")))
+        return True
+    except:
+        print "Error: " + site + " failed"
+        errors = errors + 1
+        return False
+
+test_site("http://myaccount.arch.tamu.edu")
+test_site("http://chc.arch.tamu.edu")
+test_site("http://chsd.arch.tamu.edu")
+test_site("http://chud.arch.tamu.edu")
+test_site("http://colonias.arch.tamu.edu")
+test_site("http://cosc.arch.tamu.edu")
+test_site("http://creativity.arch.tamu.edu")
+test_site("http://crs.arch.tamu.edu")
+test_site("http://dept.arch.tamu.edu")
+test_site("http://hrrc.arch.tamu.edu")
+test_site("http://laup.arch.tamu.edu")
+test_site("http://one.arch.tamu.edu")
+test_site("http://payments.arch.tamu.edu")
+
+# pypi.arch does not have a footer class
+# so we test for the container and table tag.
+driver.get("http://pypi.arch.tamu.edu")
+try:
+    WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "container")))
+    WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.TAG_NAME, "table")))
+except:
+    print "pypi.arch failed"
+
+test_site("http://symposium.arch.tamu.edu")
+test_site("http://targetcities.arch.tamu.edu")
+test_site("http://viz.arch.tamu.edu")
+test_site("http://www.arch.tamu.edu")
+
+driver.quit()
+
+endMillis = int(round(time.time() * 1000))
+totalMillis = endMillis - startMillis
+print "Test time: " + str(float(totalMillis)/1000) + " seconds"
+
+print "Total errors: " + str(errors) + " errors"
+
+if errors > 0:
+    sys.exit(2)
+
